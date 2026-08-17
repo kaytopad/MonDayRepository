7b84ea607c628935296d9a172c593a6c747fb26d
kaytopod0119 <137128977+kaytopad@users.noreply.github.com>
2026-08-17 11:58:12 +0900

    追加

--- 20260817/2026_Summer_Task4_function.cpp
+++ 20260817/2026_Summer_Task4_function.cpp
@@ -4,3 +4,75 @@
 #include "2026_Summer_Task4_header.h"
 
 using namespace std;
+
+//乱数初期
+//srand((unsigned int)time(NULL));
+
+void InitializeBoard(bool board[])
+{
+	int i;
+	for (i = 0; i < NUM_BOARDS; i++)
+	{
+		board[i] = true;
+	}
+
+}
+
+void RollDice(int dice[])
+{
+	srand((unsigned int)time(NULL));
+
+	dice[0] = rand() % 6 + 1;
+	dice[1] = rand() % 6 + 1; 
+	dice[2] = dice[0] + dice[1];
+}
+
+void DisplayDiceResults(int dice[])
+{
+	cout << "さいころの結果: " << dice[0] << " と " << dice[1] << " (合計: " << dice[2] << ")" << endl;
+
+}
+
+bool CanSelectAnyBoard(bool board[], int dice[])
+{
+	bool canSelect = false;
+	for (int i = 0; i < NUM_DICE_TOTAL; i++)
+	{
+		if (dice[i] < 1 || dice[i] > NUM_BOARDS)
+		{
+			continue;
+		}
+		if (board[dice[i] -1])
+		{
+			canSelect = true;
+		}
+	}
+
+	return canSelect;
+}
+
+int SelectBoard(bool board[], int dice[])
+{
+	int choice;
+	while (true)
+	{
+		cin >> choice;
+
+		for (int i = 0; i < 3; i++)
+		{
+			if (choice == dice[i] &&choice >= 1 && choice <= NUM_BOARDS &&board[choice - 1])
+			{
+				break;
+			}
+		}
+
+		cout << "入力に誤りがあります。再度入力してください。" << endl;
+	}
+	
+	return choice;
+}
+
+int SelectPlayerBoard(bool board[], int dice[])
+{
+	
+}
--- 20260817/2026_Summer_Task4_header.h
+++ 20260817/2026_Summer_Task4_header.h
@@ -9,6 +9,9 @@ const int NUM_BOARDS = 9;
 const int NUM_DICE = 6;
 //振るさいころの数
 const int NUM_DICE_ROLL = 2;
+//確認するさいころの総数
+const int NUM_DICE_TOTAL = NUM_DICE_ROLL + 1; 
+
 
 
 //================================================================
@@ -26,16 +29,67 @@ void StartGame();
 /// <param name="board">取得マスのフラグ</param>
 void InitializeBoard(bool board[]);
 
-//さいころを振る
-//さいころの結果を表示
-//取得できるマスがあるか確認
-//取得するマスを選択
-//プレイヤーが取得するマスを選択
-//CPUが取得するマスを選択
-//マスを取得
-//全てのマスが取得されたか確認
-//取得可能なマスを表示
+/// <summary>
+/// さいころを振る
+/// </summary>
+/// <param name="dice">さいころの結果</param>
+void RollDice(int dice[]);
 
+/// <summary>
+/// さいころの結果を表示
+/// </summary>
+/// <param name="dice">さいころの結果</param>
+void DisplayDiceResults(int dice[]);
 
+/// <summary>
+/// 取得できるマスがあるか確認
+/// </summary>
+/// <param name="board">ボードの状態</param>
+/// <param name="dice">さいころの結果</param>
+/// <returns>取得できるマスがある場合true、ない場合false</returns>
+bool CanSelectAnyBoard(bool board[], int dice[]);
 
+/// <summary>
+/// 取得するマスを選択
+/// </summary>
+/// <param name="board">ボードの状態</param>
+/// <param name="dice">さいころの結果</param>
+/// <returns>選択されたマスのインデックス</returns>
+int SelectBoard(bool board[], int dice[]);
+
+/// <summary>
+/// プレイヤーが取得するマスを選択
+/// </summary>
+/// <param name="board">ボードの状態</param>
+/// <param name="dice">さいころの結果</param>
+/// <returns>選択されたマスのインデックス</returns>
+int SelectPlayerBoard(bool board[], int dice[]);
+
+/// <summary>
+/// CPUが取得するマスを選択
+/// </summary>
+/// <param name="board">ボードの状態</param>
+/// <param name="dice">さいころの結果</param>
+/// <returns>選択されたマスのインデックス</returns>
+int SelectCPUBoard(bool board[], int dice[]);
+
+/// <summary>
+/// マスを取得
+/// </summary>
+/// <param name="board">ボードの状態</param>
+/// <param name="index">取得するマスのインデックス</param>
+void ClaimBoard(bool board[], int index);
+
+/// <summary>
+/// 全てのマスが取得されたか確認
+/// </summary>
+/// <param name="board">ボードの状態</param>
+/// <returns>全てのマスが取得された場合true、そうでない場合false</returns>
+bool IsAllBoardsClaimed(bool board[]);
+
+/// <summary>
+/// 取得可能なマスを表示
+/// </summary>
+/// <param name="board">ボードの状態</param>
+void DisplayAvailableBoards(bool board[]);
 
