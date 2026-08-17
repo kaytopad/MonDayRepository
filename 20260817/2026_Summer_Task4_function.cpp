80a9609d3214ec77dbe311c8ca4b644036cd43b2
yui <t.yui@vikingmaxx.com>
2026-08-17 10:24:16 +0900

    夏休み課題

---
+++ 20260817/2026_Summer_Task4_function.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "2026_Summer_Task4_header.h"
+
+using namespace std;
---
+++ 20260817/2026_Summer_Task4_main.cpp
@@ -0,0 +1,8 @@
+#include "2026_Summer_Task4_header.h"
+
+int main(void)
+{
+	//ƒQ[ƒ€ŠJn
+	StartGame();
+	return 0;
+}
